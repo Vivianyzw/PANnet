91289fb2af58bfbf417b0447860dd5a65c33dc51
Vivian <vivianziweiyang@gmail.com>
2019-11-01 16:45:27 +0800

    fix the bug

--- data_loader.py
+++ data_loader.py
@@ -9,6 +9,9 @@ import traceback
 import pyclipper
 import cv2
 
+# 现在假设图片及标签形式为. filename1.jpg ,filename1.txt
+# filename1.txt 内容为， x1, y1, x2, y2, x3, y3, x4, y4
+
 
 def make_dataset(img_path, label_path):
     dataset = []
@@ -21,7 +24,7 @@ def make_dataset(img_path, label_path):
     return dataset
 
 
-class DataLoader(DATA.Dataset):
+class mytraindata(DATA.Dataset):
     def __init__(self, img_path, label_path, transform=None, rescale=None):
         super(mytraindata, self).__init__()
         self.train_set_path = make_dataset(img_path, label_path)
@@ -37,16 +40,26 @@ class DataLoader(DATA.Dataset):
         input_size = image.shape[:-1]
         polys, tags = check_and_validate_polys(polys, tags, input_size)
         gt_maps, seg_maps, training_mask = generate_seg(input_size, polys, tags, self.scale_ratio)
+
         transform = transforms.ToTensor()
         image = cv2.resize(image, (640, 640))
         gt_maps = cv2.resize(gt_maps, (640, 640))
         seg_maps = cv2.resize(seg_maps, (640, 640))
         training_mask = cv2.resize(training_mask, (640, 640))
-        
+
         if self.transform:
             image = transform(image)
 
         return image, gt_maps, seg_maps, training_mask
+
+    def show_result(self, image, polys, tags):
+        for poly, tag in zip(polys, tags):
+            cv2.line(image, (poly[0][0],poly[0][1]), (poly[1][0], poly[1][1]),(0,255,0),1)
+            cv2.line(image, (poly[0][0],poly[0][1]), (poly[3][0], poly[3][1]),(0,255,0),1)
+            cv2.line(image, (poly[2][0], poly[2][1]), (poly[1][0], poly[1][1]), (0, 255, 0), 1)
+            cv2.line(image, (poly[2][0], poly[2][1]), (poly[3][0], poly[3][1]), (0, 255, 0), 1)
+        cv2.imshow('', image)
+        cv2.waitKey()
 
     def __len__(self):
         return len(self.train_set_path)
@@ -65,6 +78,7 @@ def load_annotation(p):
             label = ','.join(labels[8:])
             x1, y1, x2, y2, x3, y3, x4, y4 = int(x1), int(y1), int(x2), int(y2), int(x3), int(y3), int(x4), int(y4)
             text_polys.append([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
+            # text_tags.append(False)
             if label == '*' or label == '###' or label == '?':
                 text_tags.append(True)
             else:
@@ -89,7 +103,7 @@ def check_and_validate_polys(polys, tags, input_size):
     validated_polys = []
     validated_tags = []
     for poly, tag in zip(polys, tags):
-        if abs(pyclipper.Area(poly))<1:
+        if abs(pyclipper.Area(poly)) < 1:
             continue
         #clockwise
         if pyclipper.Orientation(poly):
@@ -241,7 +255,7 @@ def generate_seg(im_size, polys, tags, scale_ratio):
             count += 1
             for shrinked_poly in shrinked_polys:
                 seg_map = cv2.fillPoly(seg_map, [np.array(shrinked_poly).astype(np.int32)], count)
-                gt_map = cv2.fillPoly(seg_map, [np.array(shrinked_poly).astype(np.int32)], 1)
+                gt_map = cv2.fillPoly(gt_map, [np.array(shrinked_poly).astype(np.int32)], 1)
 
         seg_maps[..., i] = seg_map
         gt_maps[..., i] = gt_map
@@ -249,30 +263,13 @@ def generate_seg(im_size, polys, tags, scale_ratio):
 
 
 if __name__ == '__main__':
-    img_path = r'D:\work\vivian\data\ch4_training_images'
-    label_path = r'D:\work\vivian\data\ch4_training_localization_transcription_gt'
-    # labelname = 'gt_img_1.txt'
-    # scale_ratio = [1.0, 0.5]
-    # image = cv2.imread(os.path.join(img_path, 'img_1.jpg'))
+    img_path = r'D:\work\vivian\data\train\ch4_training_images'
+    label_path = r'D:\work\vivian\data\train\ch4_training_localization_transcription_gt'
 
-    # polys, tags = load_annotation(os.path.join(label_path, labelname))
-    # image, polys, tags = crop_area(image, polys, tags)
-    # input_size = image.shape[:-1]
-    # polys, tags = check_and_validate_polys(polys, tags, input_size)
-    # gt_maps, seg_maps, training_mask = generate_seg(input_size, polys, tags, 'a', scale_ratio)
-    # print(seg_maps)
-    # print(seg_maps.shape)
-    # print(np.max(seg_maps))
-    # seg_map = seg_maps[:, :, 1]*100
-    # cv2.imshow('', seg_map)
-    # cv2.waitKey()
     dataset = mytraindata(img_path, label_path, True, True)
+
     data_loader = DATA.DataLoader(dataset, batch_size=1)
     for i, data in enumerate(data_loader, 0):
         image, gt_maps, seg_maps, training_mask = data
-        gt_map = gt_maps[:,:,:,0]
 
-        print(gt_maps.shape)
-        cv2.imshow('', gt_map)
-        cv2.waitKey()
 
